158b6839cfa5c320cd045b6098885a389153510f
Ivan Lytovka <ivanlytovka@gmail.com>
2019-11-25 20:36:25 -0500

    Updates

---
+++ precache-manifest.645e62365cdc545f1680f130121ea626.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd969b7a5917f1c45e5a02560402400",
+    "url": "/lytovka/term-project.git/index.html"
+  },
+  {
+    "revision": "95adf4b092d9afb4b183",
+    "url": "/lytovka/term-project.git/static/css/main.72357865.chunk.css"
+  },
+  {
+    "revision": "dd1216a8f42657c87333",
+    "url": "/lytovka/term-project.git/static/js/2.09a167f5.chunk.js"
+  },
+  {
+    "revision": "95adf4b092d9afb4b183",
+    "url": "/lytovka/term-project.git/static/js/main.27a24813.chunk.js"
+  },
+  {
+    "revision": "b2fbab933aae7dfee77d",
+    "url": "/lytovka/term-project.git/static/js/runtime-main.0d7c7093.js"
+  },
+  {
+    "revision": "d9fcc607d6e9019286b691f9a084f4da",
+    "url": "/lytovka/term-project.git/static/media/mongoFull.d9fcc607.png"
+  },
+  {
+    "revision": "2c516de8695833693d8b34764877989b",
+    "url": "/lytovka/term-project.git/static/media/ratingReact.2c516de8.png"
+  },
+  {
+    "revision": "657da7d85651e3638b5cae2e14a41c45",
+    "url": "/lytovka/term-project.git/static/media/reactFull.657da7d8.png"
+  }
+]);
